6bef020d80846c4f958a9a9bc540bc76391c4642
JTPham45 <jpham45@ymail.com>
2023-03-09 00:35:34 -0600

    Further service impl

--- clothingstore/src/main/java/com/ics499/clothingstore/serviceImp/CustomerServiceImp.java
+++ clothingstore/src/main/java/com/ics499/clothingstore/serviceImp/CustomerServiceImp.java
@@ -1,16 +1,31 @@
 package com.ics499.clothingstore.serviceImp;
 
-import javax.persistence.EntityManager;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.ics499.clothingstore.model.Customer;
 import com.ics499.clothingstore.service.CustomerService;
 
 public class CustomerServiceImp implements CustomerService{
 
-//	EntityManager emp = new EntityManager();
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.isc499.clothingstore");
+	EntityManager em = emf.createEntityManager();
 	
 	@Override
 	public boolean isValid(String username, String password) {
 		// TODO Auto-generated method stub
+		
+		int customer = em.createQuery("SELECT Customer from Customer customer where customer.id = ?id").getFirstResult();
+		
+		/*
+		 * logic- 
+		 * 
+		 * if ((Customer.getPassword.equals(password)&&(customer.getUsername = username))
+		 * return true;
+		 */		
 		return false;
 	}
 
