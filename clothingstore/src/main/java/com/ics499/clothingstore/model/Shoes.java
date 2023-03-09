4c322a04e29371ca2fc03b4605f3b8b2dd32e9b0
Dylan Skokan <36804815+DylanSkokan@users.noreply.github.com>
2023-03-09 09:36:06 -0600

    unique attributes
    - added attributes to each child of Product to make them unique
    - changed some attributes (such as Product product) to an id version (such as int productId) for later table linking
    - added .class files to git ignore

--- clothingstore/src/main/java/com/ics499/clothingstore/model/Shoes.java
+++ clothingstore/src/main/java/com/ics499/clothingstore/model/Shoes.java
@@ -5,15 +5,27 @@ import javax.persistence.Entity;
 @Entity
 public class Shoes extends Product {
 	
-	//@GeneratedValue
-	//private long shoesId = 0;
-
-	public Shoes(float price, int stock, String description, String brand, String color, String size, String fit) {
+	private enum ShoesType {
+		sneakers, dress, sandles, boots, slippers
+	}
+	
+	private ShoesType type;
+	
+	public Shoes(float price, int stock, String description, String brand, String color, String size, String fit,
+			ShoesType type) {
 		super(price, stock, description, brand, color, size, fit);
-		// TODO Auto-generated constructor stub
+		this.type = type;
 	}
 
 	public Shoes() {
 
+	}
+
+	public ShoesType getType() {
+		return type;
+	}
+
+	public void setType(ShoesType type) {
+		this.type = type;
 	}
 }
