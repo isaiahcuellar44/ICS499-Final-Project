96b2c3c193599cf75cadfacf0ff269eae76d4dda
JTPham45 <jpham45@ymail.com>
2023-03-08 20:33:37 -0600

    Service Implementation

--- clothingstore/src/main/java/com/ics499/clothingstore/serviceImp/CustomerServiceImp.java
+++ clothingstore/src/main/java/com/ics499/clothingstore/serviceImp/CustomerServiceImp.java
@@ -1,5 +1,17 @@
 package com.ics499.clothingstore.serviceImp;
 
-public class CustomerServiceImp {
+import javax.persistence.EntityManager;
+
+import com.ics499.clothingstore.service.CustomerService;
+
+public class CustomerServiceImp implements CustomerService{
+
+//	EntityManager emp = new EntityManager();
+	
+	@Override
+	public boolean isValid(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
