0340d4d94357c0fc7735a2c798395cb0f0b7e563
Dylan Skokan <36804815+DylanSkokan@users.noreply.github.com>
2023-04-10 19:53:41 -0500

    added cross origins

--- clothingstore/src/main/java/com/ics499/clothingstore/controller/UserController.java
+++ clothingstore/src/main/java/com/ics499/clothingstore/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ics499.clothingstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.ics499.clothingstore.repository.UserRepository;
 
 @RestController
 @RequestMapping("user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	@Autowired
