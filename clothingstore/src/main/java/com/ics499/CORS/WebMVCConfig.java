6701e9931dfe0db35f300ea255e3eb4f89708f1b
isaiahcuellar44 <77817734+isaiahcuellar44@users.noreply.github.com>
2023-04-16 20:34:05 -0500

    FIxing CORS network issues
    This allows cross data between the front end and back end application.

---
+++ clothingstore/src/main/java/com/ics499/CORS/WebMVCConfig.java
@@ -0,0 +1,17 @@
+package com.ics499.CORS;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // replace with your Angular app URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
